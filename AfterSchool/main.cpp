a2026be1f17a0c3c6865f52febb00eba16535a14
ASJW <AS@JW>
2022-12-26 15:05:15 +0900

    몬스터 제거기능 추가

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <SFML/Graphics.hpp>
-
 using namespace sf;
-
 int main(void)
 {
-	// 윈도창 생성
+	//윈도우창 생성
 	RenderWindow window(VideoMode(640, 480), "AfterSchool");
 	window.setFramerateLimit(60);
-
 	RectangleShape player;
 	player.setSize(Vector2f(40, 40));
 	player.setPosition(100, 100);
@@ -19,50 +16,46 @@ int main(void)
 	enemy.setSize(Vector2f(70, 70));
 	enemy.setPosition(500, 300);
 	enemy.setFillColor(Color::Yellow);
+	int enemy_life = 1;
 
-
-	// 윈도가 열려있을 때까지 반복
+	//윈도우가 열려있을 때까지 반복
 	while (window.isOpen())
 	{
 		Event event;
-		while (window.pollEvent(event))
-		{
+		while (window.pollEvent(event)) {
+			//종료(X) 버튼을 누르면
 			switch (event.type)
 			{
-				// 종료(x) 버튼을 누르면 Event::Closed(0)
 			case Event::Closed:
-				window.close();		// 윈도를 닫는다
+				window.close();//윈도우 닫는다.
 			}
 		}
 
-		if (Keyboard::isKeyPressed(Keyboard::Left))
-		{
+		if (Keyboard::isKeyPressed(Keyboard::Left)) {
 			player.move(-player_speed, 0);
 		}
-		if (Keyboard::isKeyPressed(Keyboard::Right))
-		{
+		if (Keyboard::isKeyPressed(Keyboard::Right)) {
 			player.move(player_speed, 0);
 		}
-		if (Keyboard::isKeyPressed(Keyboard::Up))
-		{
+		if (Keyboard::isKeyPressed(Keyboard::Up)) {
 			player.move(0, -player_speed);
 		}
-		if (Keyboard::isKeyPressed(Keyboard::Down))
-		{
+		if (Keyboard::isKeyPressed(Keyboard::Down)) {
 			player.move(0, player_speed);
 		}
-
-		if (player.getGlobalBounds().intersects(enemy.getGlobalBounds()))
-		{
-			printf("enemy과 충돌\n");
+		if (enemy_life > 0) {
+			if (player.getGlobalBounds().intersects(enemy.getGlobalBounds()));
+			{
+				printf("enemy와 충돌 \n");
+				enemy_life -= 1;
+			}
 		}
 
 		window.clear(Color::Black);
-
-		// draw는 나중에 호출할수록 우선순위가 높아짐
-		window.draw(enemy);
+		if (enemy_life > 0) {
+			window.draw(enemy);
+		}
 		window.draw(player);
-
 		window.display();
 	}
 
